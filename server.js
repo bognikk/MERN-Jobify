38307cdc236913b1daa9b89a38e636c61dc39433
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-21 11:41:35 -0400

    dependencies, express and nodemon

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("server is working");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.listen(5100, () => {
+	console.log("server running");
+});
