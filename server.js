76cacd48c4fb992ff4ecb9bfac32a452384266d4
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-21 17:15:34 -0400

    morgan and dotenv

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Hello world");
 });
 
-app.listen(5100, () => {
-	console.log("server running");
+app.post("/", (req, res) => {
+	res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+	console.log(`server running on PORT ${port}`);
 });
