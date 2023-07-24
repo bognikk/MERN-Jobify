6f0d98e18db8d22d6298444675cf41326dbfc4e7
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-07-24 17:31:52 -0400

    get single job

--- server.js
+++ server.js
@@ -38,6 +38,16 @@ app.post("/api/v1/jobs", (req, res) => {
 	const id = nanoid(10);
 	const job = { id, company, position }; // { id: id, company: company...}
 	jobs.push(job);
+	res.status(201).json({ job });
+});
+
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+	const { id } = req.params;
+	const job = jobs.find((job) => job.id === id);
+	if (!job) {
+		return res.status(404).json({ msg: `no job with id ${id}` });
+	}
 	res.status(200).json({ job });
 });
 
