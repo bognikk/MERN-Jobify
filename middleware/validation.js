b7713de9eb67aec12c821d9ec1efcc39974379fc
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-04 09:14:00 -0400

    validate register user

--- middleware/validation.js
+++ middleware/validation.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from "../errors/custom-errors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/job-model.js";
+import User from "../models/user-model.js";
 
 const withValidationErrors = (validateValues) => {
 	return [
@@ -42,4 +43,26 @@ export const validateIdParam = withValidationErrors([
 			throw new NotFoundError(`no job with id : ${value}`);
 		}
 	}),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+	body("name").notEmpty().withMessage("name is required"),
+	body("email")
+		.notEmpty()
+		.withMessage("email is required")
+		.isEmail()
+		.withMessage("invalid email format")
+		.custom(async (email) => {
+			const user = await User.findOne({ email });
+			if (user) {
+				throw new BadRequestError("email already exists");
+			}
+		}),
+	body("password")
+		.notEmpty()
+		.withMessage("password is required")
+		.isLength({ min: 8 })
+		.withMessage("password must be at least 8 characters long"),
+	body("location").notEmpty().withMessage("location is required"),
+	body("lastName").notEmpty().withMessage("last name is required"),
 ]);
