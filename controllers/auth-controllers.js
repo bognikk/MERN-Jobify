f3e6c21b55481104e94fa62f96ef16410bbb1a8d
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-04 22:14:53 -0400

    hash password

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,11 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/user-model.js";
 
+import bcrypt from "bcryptjs";
+
 export const register = async (req, res) => {
 	const isFirstAccount = (await User.countDocuments()) === 0;
 	req.body.role = isFirstAccount ? "admin" : "user";
+
+	const hashedPassword = await bcrypt.hash(req.body.password, 12);
+	req.body.password = hashedPassword;
+
 	const user = await User.create(req.body);
-	res.status(StatusCodes.CREATED).json({ user });
+	res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
